779376ff7a12ef189601b8cc2ec783069be25684
Travis Dowd <teaskittle@gmail.com>
2020-03-23 18:34:15 -0600

    Add mkdir.c

---
+++ src/mkdir.c
@@ -0,0 +1,17 @@
+// Make new directory argv[1]
+// Author: Travis Dowd
+// Date: 3-23-2020
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main ( int argc, char *argv[] ) {
+  struct stat st = {0};
+  if ( argc == 2 ) {
+    if ( stat ( argv[1], &st ) == -1 ) {
+      mkdir( argv[1], 0777 ); }
+  } else {
+    perror( "mkdir" );
+  } return 0;
+}
