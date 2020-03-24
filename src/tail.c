76df0ca62d4806df957d94f7c75b70157cf1dad2
Travis Dowd <teaskittle@gmail.com>
2020-03-23 18:03:50 -0600

    Add head.c

---
+++ src/tail.c
@@ -0,0 +1,26 @@
+// print last 10 lines of file
+// Author: Travis Dowd
+// Date: 3-23-2020
+
+#include  <stdio.h>
+
+int main( int argc, char *argv[] ) {
+  int lines = 0;
+  int rev_lines = 0;
+  if ( argc == 2 ) {
+    static int c;
+    FILE *f = fopen( argv[1], "r" );
+    if ( f ) {
+      while (( c = getc( f )) != EOF ) {      
+	if ( c == '\n' ) { lines++; }
+      } rewind( f );
+      printf( "\x1b[0;72A" ); // Move cursor up one
+      while (( c = getc( f )) != EOF ) {      
+	if ( c == '\n' ) { rev_lines++; }
+	if ( rev_lines > ( lines - 11 )) { putchar( c ); }
+      } fclose( f ); 
+    } else {
+      printf( "Can't open file\n" );
+    }
+  } return 0;
+}
