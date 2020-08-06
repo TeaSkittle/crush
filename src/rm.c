144954e059ca851d626548e26fe81e79b76f517a
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 14:21:04 -0600

    Update rm.c

--- src/rm.c
+++ src/rm.c
@@ -4,10 +4,9 @@
 
 #include <stdio.h>
 
-int main( int argc, char *argv[] ){
+int main( int argc, char *argv[] ) {
   if ( argc >= 1 ) {
     for ( int i = 1; i < argc; i++ ) {
-      remove( argv[i] );
-    }
+      remove( argv[i] ); }
   } return 0;
 }
