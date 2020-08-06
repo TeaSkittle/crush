60424d44ab8b39c9061ccb23af8fda4e09397998
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 14:16:32 -0600

    Update mv.c

--- src/mv.c
+++ src/mv.c
@@ -6,18 +6,16 @@
 
 int main( int argc, char *argv[] ) {
   if ( argc == 3 ) {
-      static char ch;
+      static char c;
       FILE *file1 = fopen( argv[1], "r" );
       FILE *file2 = fopen( argv[2], "w" );
       if ( file1 ) {
-      	while( (ch = fgetc(file1)) != EOF ) {
-	  fputc( ch, file2 );
-	} fclose(file1);
-      fclose(file2);
-      remove ( argv[1] );
-      }
+      	while ((c = fgetc( file1 )) != EOF ) {
+	  fputc( c, file2 ); }
+	fclose( file1 );
+     	fclose( file2 );
+      	remove( argv[1] ); }
   } else {
     perror( "mv" );
   } return 0;
 }
-
