aebbd96b401974b456513ebc91659c8cbf4444b3
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 13:23:32 -0600

    Update cp.c

--- src/cp.c
+++ src/cp.c
@@ -7,13 +7,13 @@
 int main( int argc, char *argv[] ) {
   if ( argc == 3 ) {
      static char ch;
-     FILE *file1 = fopen ( argv[1], "r" );
-     FILE *file2 = fopen ( argv[2], "w" );
+     FILE *file1 = fopen( argv[1], "r" );
+     FILE *file2 = fopen( argv[2], "w" );
      if ( file1 ) {
        while (( ch = fgetc( file1 )) != EOF ) {
 	  fputc( ch, file2 ); }
-      fclose(file1);
-      fclose(file2);
+    	fclose( file1 );
+      	fclose( file2 );
      } return 0;
   } else {
     perror( "cp" );
