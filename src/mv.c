f7f5c0c5b8d74cb3b3816b80cd44d43863f19a95
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 13:36:17 -0600

    Update mv.c

--- src/mv.c
+++ src/mv.c
@@ -10,10 +10,9 @@ int main( int argc, char *argv[] ) {
       FILE *file1 = fopen( argv[1], "r" );
       FILE *file2 = fopen( argv[2], "w" );
       if ( file1 ) {
-      while( (ch = fgetc(file1)) != EOF ) {
+      	while( (ch = fgetc(file1)) != EOF ) {
 	  fputc( ch, file2 );
-	}
-      fclose(file1);
+	} fclose(file1);
       fclose(file2);
       remove ( argv[1] );
       }
