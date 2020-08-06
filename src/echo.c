2c1f211aca16bdd3b53fa493add2702ec09fd29e
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 13:27:38 -0600

    Update echo.c

--- src/echo.c
+++ src/echo.c
@@ -5,9 +5,10 @@
 #include <stdio.h>
 
 int main( int argc, char *argv[] ) {
-  int i;
+  // Why would I do this?!?!?!
+  //int i;
   if ( argc > 0 ) {
-    for ( i = 1; i < argc; i++ ) {
+    for ( int i = 1; i < argc; i++ ) {
       printf( "%s ", argv[i] );
       if ( i == argc - 1 ) { printf( "\n" ); }
     }
