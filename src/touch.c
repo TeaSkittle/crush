13ba587846b7d86d2d756368414dcdc2be08598e
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 14:22:16 -0600

    Update touch.c

--- src/touch.c
+++ src/touch.c
@@ -10,10 +10,10 @@ int main( int argc, char *argv[] ) {
   //int ch = '\0';
   int ch = 0;
   if ( argc > 0 ) {
-    FILE *f = fopen ( argv[1], "w" );
+    FILE *f = fopen( argv[1], "w" );
     if ( f ) {
       fputc( ch, f );
-    } fclose ( f );
+    } fclose( f );
   } else {
     perror( "touch" );
   } return 0; // returning 139?
