92206896f8bb585d27611e00ba0307ed883f1311
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 13:29:17 -0600

    Update echo.c

--- src/echo.c
+++ src/echo.c
@@ -10,7 +10,9 @@ int main( int argc, char *argv[] ) {
   if ( argc > 0 ) {
     for ( int i = 1; i < argc; i++ ) {
       printf( "%s ", argv[i] );
-      if ( i == argc - 1 ) { printf( "\n" ); }
+      // the argc-- might be better
+      //if ( i == argc - 1 ) { printf( "\n" ); }
+      if ( i == argc-- ) { printf( "\n" ); }
     }
   } else {
     printf( "\n" );
