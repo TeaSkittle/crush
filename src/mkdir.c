3f71bcd1463a3da4a18fa0fd50dc9903abf8d6df
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-08-06 13:35:22 -0600

    Update mkdir.c

--- src/mkdir.c
+++ src/mkdir.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 
 int main ( int argc, char *argv[] ) {
-  struct stat st = {0};
+  struct stat st = { 0 };
   if ( argc == 2 ) {
     if ( stat ( argv[1], &st ) == -1 ) {
       mkdir( argv[1], 0777 ); }
